0f46cdf92265afd53a87807daa847dfaaea8e9a2
MrAndreID <MrAndreID@outlook.com>
2024-04-13 21:21:30 +0700

    improve models (change data type for created at, updated at, and deleted at)

--- applications/databases/models/email.go
+++ applications/databases/models/email.go
@@ -8,9 +8,9 @@ import (
 
 type Email struct {
 	ID        string         `gorm:"primaryKey;Column:id;type:varchar(45)" json:"id"`
-	CreatedAt time.Time      `gorm:"Column:created_at;type:timestamp;not null" json:"createdAt"`
-	UpdatedAt time.Time      `gorm:"Column:updated_at;type:timestamp;not null" json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"Column:deleted_at;type:timestamp" json:"deletedAt"`
+	CreatedAt time.Time      `gorm:"Column:created_at;type:timestamptz;not null" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"Column:updated_at;type:timestamptz;not null" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"Column:deleted_at;type:timestamptz" json:"deletedAt"`
 	UserID    string         `gorm:"Column:user_id;type:varchar(45);not null" json:"userId"`
 	Email     string         `gorm:"Column:email;type:varchar(255);not null" json:"email"`
 }
--- applications/databases/models/user.go
+++ applications/databases/models/user.go
@@ -8,9 +8,9 @@ import (
 
 type User struct {
 	ID        string         `gorm:"primaryKey;Column:id;type:varchar(45)" json:"id"`
-	CreatedAt time.Time      `gorm:"Column:created_at;type:timestamp;not null" json:"createdAt"`
-	UpdatedAt time.Time      `gorm:"Column:updated_at;type:timestamp;not null" json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"Column:deleted_at;type:timestamp" json:"deletedAt"`
+	CreatedAt time.Time      `gorm:"Column:created_at;type:timestamptz;not null" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"Column:updated_at;type:timestamptz;not null" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"Column:deleted_at;type:timestamptz" json:"deletedAt"`
 	Name      string         `gorm:"Column:name;type:varchar(255);not null" json:"name"`
 	Emails    []Email        `gorm:"foreignKey:UserID;references:ID" json:"emails"`
 }
